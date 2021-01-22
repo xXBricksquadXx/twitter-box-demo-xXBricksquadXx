115d67820934c14ae880a529f28463eee908c522
Colby Cardell <Colby88cardell@gmail.com>
2021-01-22 00:01:57 -0500

    Named multiple Exports-Made Code more declarative and modular !

--- lib.js
+++ lib.js
@@ -4,9 +4,25 @@ const form = document.querySelector("form");
 const span = form.querySelector("span");
 
 /* Object */
-export default {
+export const dom = {
   button: form.querySelector("button"),
   charCountSpan: span.querySelector("span"),
   span, // short hand for span:span
   textarea: form.querySelector("textarea"),
+};
+
+export const toggleButton = (count) => {
+  if (count) {
+    dom.button.disabled = false;
+  } else {
+    dom.button.disabled = true;
+  }
+};
+
+export const toggleCharCountError = (count) => {
+  if (count > 140) {
+    dom.span.classList.add("is-error");
+  } else {
+    dom.span.classList.remove("is-error");
+  }
 };
--- scripts.js
+++ scripts.js
@@ -1,18 +1,11 @@
-import dom from "./lib.js";
+// destructure dom
+
+import { dom, toggleButton, toggleCharCountError } from "./lib.js";
 
 dom.textarea.addEventListener("keyup", function handleTextarea() {
   const charCount = this.value.length;
-  if (charCount) {
-    dom.button.disabled = false;
-  } else {
-    dom.button.disabled = true;
-  }
 
   dom.charCountSpan.innerText = charCount;
-
-  if (charCount > 140) {
-    dom.span.classList.add("is-error");
-  } else {
-    dom.span.classList.remove("is-error");
-  }
+  toggleButton(charCount);
+  toggleCharCountError(charCount);
 });
